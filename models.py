860de07fff43aaf3062786f97de5d2933b059ed3
mhmdrdwn <mrdwn4@gmail.com>
2022-10-25 19:07:59 +0200

    LSTM

--- models.py
+++ models.py
@@ -41,9 +41,9 @@ class ChronoNet(nn.Module):
     x=x.permute(0,2,1)
     gru_out1,_=self.gru1(x)
     gru_out2,_=self.gru2(gru_out1)
-    gru_out=torch.cat([gru_out1,gru_out2],dim=2)
+    gru_out=torch.cat([gru_out1, gru_out2],dim=2)
     gru_out3,_=self.gru3(gru_out)
-    gru_out=torch.cat([gru_out1,gru_out2,gru_out3],dim=2)
+    gru_out=torch.cat([gru_out1, gru_out2, gru_out3],dim=2)
     linear_out=self.relu(self.gru_linear(gru_out.permute(0,2,1)))
     gru_out4,_=self.gru4(linear_out.permute(0,2,1))
     x=self.flatten(gru_out4)
@@ -51,41 +51,34 @@ class ChronoNet(nn.Module):
     out = torch.sigmoid(x)
     return out
 
-class LSTM(nn.Module):
-    def __init__(self, output_size, input_size, hidden_size, num_layers, num_channels=21):
-        super(LSTM, self).__init__()
 
-        self.num_layers = num_layers
-        self.input_size = input_size
-        self.hidden_size = hidden_size
+class LSTM(nn.Module):
+    def __init__(self, input_size, num_channels, hidden_units):
+        super().__init__()
         self.num_channels = num_channels
-        self.LSTMs = []
-        for i in range(num_channels):
-            lstm = nn.LSTM(input_size=input_size, hidden_size=hidden_size,
-                            num_layers=num_layers, batch_first=True, 
-                            bidirectional=True)
-            self.LSTMs.append(lstm)
-        self.linear = nn.Linear(hidden_size*2, 8)
-        self.out_linear = nn.Linear(8*num_channels, output_size)
-        self.softmax= nn.LogSoftmax(dim=-1)
+        self.hidden_units = hidden_units
+        self.num_layers = 2
+        self.input_size = input_size
+        self.inputsize_channels = num_channels * input_size
+
+        self.lstm = nn.LSTM(
+            input_size=num_channels,
+            hidden_size=hidden_units,
+            batch_first=True,
+            num_layers=self.num_layers
+        )
+
+        self.linear = nn.Linear(in_features=self.hidden_units, out_features=1)
+        self.enc = nn.Linear(in_features=self.inputsize_channels, out_features=100*num_channels)
 
     def forward(self, x):
-        ch_out = []
-        
-        for ch_idx in range(self.num_channels):
-            x_ch = x[:, ch_idx, :]
-            x_ch = x_ch.unsqueeze(1)
-            h0 = Variable(torch.zeros(self.num_layers*2, x.size(0), 
-                self.hidden_size))
-            c0 = Variable(torch.zeros(self.num_layers*2, x.size(0), 
-                self.hidden_size))
-            lstm_out, (hn, cn) = self.LSTMs[ch_idx](x_ch, (h0, c0))
-            #out = lstm_out[:, -1, :]
-            out = hn.view(-1, self.hidden_size*2)
-            out = self.linear(out)
-            ch_out.append(out)
-        #print(out.shape)
-        ch_out = torch.cat(ch_out, dim=1)
-        out = self.out_linear(ch_out)
-        #out = self.softmax(out)
+        batch_size = x.shape[0]
+        x = x.view(batch_size, x.shape[1]*x.shape[2])
+        x = self.enc(x)
+        x = x.view(batch_size, int(x.shape[-1]/self.num_channels), self.num_channels)
+        h0 = torch.zeros(self.num_layers, batch_size, self.hidden_units).requires_grad_()
+        c0 = torch.zeros(self.num_layers, batch_size, self.hidden_units).requires_grad_()
+        lstm_out, (hn, _) = self.lstm(x, (h0, c0))
+        out = self.linear(hn[0]).flatten()
+        out = torch.sigmoid(out)
         return out
