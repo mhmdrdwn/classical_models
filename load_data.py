57047f25bda7b2a6c4744580cf4471fd59c753dc
mhmdrdwn <mrdwn4@gmail.com>
2022-10-27 15:28:50 +0200

    load data

--- load_data.py
+++ load_data.py
@@ -1,20 +1,21 @@
 import numpy as np
 from tqdm import tqdm
 import glob
+import math 
 import random
 import torch
 
 from utils import StandardScaler3D
 
 
-def data_file_names():
+def data_file_names(sample_size):
     test_abnormal_files = glob.glob('./tuh/eval/abnormal/*.npy')
     test_normal_files = glob.glob('./tuh/eval/normal/*.npy')
     normal_files = glob.glob('./tuh/train/normal/*.npy')
     abnormal_files = glob.glob('./tuh/train/abnormal/*.npy')
-
-    sample_size = 400
-    train_data_size = 300
+    
+    np.random.seed(42)
+    train_data_size = math.floor(sample_size*3/4)
     normal_files = random.sample(normal_files, sample_size)
     abnormal_files = random.sample(abnormal_files, sample_size)
 
@@ -50,13 +51,22 @@ def read_data_arrays(data_file_names):
     val_abnormal_features=[np.load(f) for f in val_abnormal_files]
     test_normal_features=[np.load(f) for f in test_normal_files]
     test_abnormal_features=[np.load(f) for f in test_abnormal_files]
-
-    train_normal_labels=[len(i)*[0] for i in train_normal_features]
-    train_abnormal_labels=[len(i)*[1] for i in train_abnormal_features]
-    val_normal_labels=[len(i)*[0] for i in val_normal_features]
-    val_abnormal_labels=[len(i)*[1] for i in val_abnormal_features]
-    test_normal_labels=[len(i)*[0] for i in test_normal_features]
-    test_abnormal_labels=[len(i)*[1] for i in test_abnormal_features]
+    
+    train_lengths = [len(x) for x in train_normal_features]
+    train_lengths = [len(x) for x in train_abnormal_features]
+    train_lengths = [len(x) for x in val_normal_features]
+    train_lengths = [len(x) for x in val_abnormal_features]
+    test_normal = [len(x) for x in test_normal_features]
+    test_abnormal = [len(x) for x in test_abnormal_features]
+    
+    test_lengths = test_normal + test_abnormal
+    
+    train_normal_labels=[len(x)*[0] for x in train_normal_features]
+    train_abnormal_labels=[len(x)*[1] for x in train_abnormal_features]
+    val_normal_labels=[len(x)*[0] for x in val_normal_features]
+    val_abnormal_labels=[len(x)*[1] for x in val_abnormal_features]
+    test_normal_labels=[len(x)*[0] for x in test_normal_features]
+    test_abnormal_labels=[len(x)*[1] for x in test_abnormal_features]
 
     train_features = train_normal_features + train_abnormal_features
     train_labels = train_normal_labels + train_abnormal_labels
@@ -76,7 +86,7 @@ def read_data_arrays(data_file_names):
     test_labels = np.hstack(test_labels)
 
     return (train_features, val_features, test_features,
-            train_labels, val_labels, test_labels)
+            train_labels, val_labels, test_labels, test_lengths)
 
 
 
@@ -88,12 +98,11 @@ def standardize_data(train_features, val_features, test_features):
     return train_features, val_features, test_features
 
 
-def data_loader(features, labels, device, batch_size):
+def data_loader(features, labels, device, batch_size, shuffle=True):
 
     features = torch.Tensor(features).float().to(device)
     labels = torch.Tensor(labels).float().to(device)
     data = torch.utils.data.TensorDataset(features, labels)
-    data_iter = torch.utils.data.DataLoader(data, batch_size, shuffle=True)
+    data_iter = torch.utils.data.DataLoader(data, batch_size, shuffle=shuffle)
 
     return data_iter
-
